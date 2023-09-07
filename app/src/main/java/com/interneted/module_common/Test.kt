ed4695f6587a7884fd790e66c89daaef3552ed4b
ed <salahayo3192@gmail.com>
2023-09-07 15:46:57 +0800

    chore: First commit

---
+++ app/src/main/java/com/interneted/module_common/Test.kt
@@ -0,0 +1,13 @@
+package com.interneted.module_common
+
+/**
+ * Creator: ED
+ * Date: 2023/9/7 15:36
+ * Mail: salahayo3192@gmail.com
+ *
+ * **/
+object Test {
+    fun addNumber(number1: Int, number2: Int): Int {
+        return number1 + number2
+    }
+}
